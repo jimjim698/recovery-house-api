c94a51a15e9efb2471070c0f465dd58bd32d8d12
Jim <jimlotruglio@yahoo.com>
2019-02-23 17:23:30 -0700

    added create action

--- app/controllers/api/clients_controller.rb
+++ app/controllers/api/clients_controller.rb
@@ -1,10 +1,24 @@
-class Api::ClientsController < ApplicationController
+require 'pry'
 
+class Api::ClientsController < ApplicationController
+skip_before_action :verify_authenticity_token
   def index
     @clients = Client.all
     render json: @clients
 
   end
 
+  def create
+    @client = Client.new(client_params)
+    if @client.save
+      render json: @client
+    end
 
+  end
+
+
+private
+  def client_params
+    params.require(:client).permit(:name, :age, :job, :chore)
+  end
 end
