cf3ce4180bc1cd8f616dd94c3ada02b93e14d711
Jim <jimlotruglio@yahoo.com>
2019-02-24 20:06:36 -0700

    fixed User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_022646) do
+ActiveRecord::Schema.define(version: 2019_02_25_030213) do
 
   create_table "clients", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_02_25_022646) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.string "autho_token"
   end
 
 end
