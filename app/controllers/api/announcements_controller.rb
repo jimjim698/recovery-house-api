5047228e48aa99863d867f6a703d79a958248355
Jim <jimlotruglio@yahoo.com>
2019-03-05 23:17:29 -0700

    set up announcement create

--- app/controllers/api/announcements_controller.rb
+++ app/controllers/api/announcements_controller.rb
@@ -1,2 +1,16 @@
 class Api::AnnouncementsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    @announcement= Announcement.create(announcement_params)
+    binding.pry
+  end
+
+
+private
+
+
+  def announcement_params
+    params.require(:announcement).permit(:content,:likes, :dislikes)
+  end 
 end
