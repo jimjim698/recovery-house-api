d2f58d117c2473094c08016e1d03ccd5d2d15bfc
Jim <jimlotruglio@yahoo.com>
2019-03-07 15:34:53 -0700

    added announcement index and finished announcement creation

--- app/controllers/api/announcements_controller.rb
+++ app/controllers/api/announcements_controller.rb
@@ -1,9 +1,19 @@
 class Api::AnnouncementsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+
+  def index
+    @announcements = Announcement.all
+    render json: @announcements
+
+  end
+
+
   def create
-    @announcement= Announcement.create(announcement_params)
-    binding.pry
+      @announcement= get_current_user.announcements.build(announcement_params)
+      @announcement.save
+      render json: @announcement
+
   end
 
 
@@ -12,5 +22,5 @@ private
 
   def announcement_params
     params.require(:announcement).permit(:content,:likes, :dislikes)
-  end 
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   #include ActionController::HttpAuthentication::Token::ControllerMethods
   # include Knock::Authenticable
 
+
+  def get_current_user
+     User.find_by(username:request.headers['HTTP_AUTHORIZATION'])
+  end 
+
+
+
+
+
 end
