bddce76da56da1577d5b13f08af785541c13cbd2
Jim <jimlotruglio@yahoo.com>
2019-03-09 21:58:44 -0700

    add uniqueness validation to users

--- config/routes.rb
+++ config/routes.rb
@@ -8,22 +8,11 @@ Rails.application.routes.draw do
     resources :users
     resources :api
     resources :announcements
+ end
 
 
- # Get login token from Knock
- post   'user_token'      => 'user_token#create'
-
- # User actions
- #get    '/users'          => 'users#index'
- #get    '/users/current'  => 'users#current'
- post   '/users/create'   => 'users#create'
- #patch  '/user/:id'       => 'users#update'
- #delete '/user/:id'       => 'users#destroy'
-  end
+   post 'api/users/login'=> 'api/users#login'
 
 
-post 'api/users/login'=> 'api/users#login'
-delete 'logout'=> 'sessions#delete'
-get '/profile'=> 'users#profile'
 
 end
